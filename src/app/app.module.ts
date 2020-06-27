c2c579ea204c13595d22d44d8d2c3ddc158bc480
henrylangman <henry.l.pta@gmail.com>
2020-06-27 12:11:06 +0200

    component scaffolding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { ContentComponent } from './content/content.component';
+import { FooterComponent } from './footer/footer.component';
+import { ContentLeftComponent } from './content/content-left/content-left.component';
+import { ContentRightComponent } from './content/content-right/content-right.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    ContentComponent,
+    FooterComponent,
+    ContentLeftComponent,
+    ContentRightComponent
   ],
   imports: [
     BrowserModule

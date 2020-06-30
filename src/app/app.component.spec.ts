b0ca7ede309c44dadcbb7436362ac31d1020f528
henrylangman <henry.l.pta@gmail.com>
2020-07-01 00:36:34 +0200

    adding standard mock example

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,18 @@
 import { async, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { Component, Input } from '@angular/core';
+import { Item } from './item.model';
+
+// example of a standard mocked component // add to declarations
+@Component({ selector: 'app-header', template: '<div></div>' })
+class MockHeaderComp {
+  @Input() item: Item;
+}
+
+@Component({ selector: 'app-content', template: '<div></div>' })
+class MockContentComp {
+  @Input() item: Item;
+}
 
 describe('AppComponent', () => {
   beforeEach(async(() => {

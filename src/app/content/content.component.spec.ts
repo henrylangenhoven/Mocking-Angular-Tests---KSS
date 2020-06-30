05febe46af526dc30abc0f1fe290d741a22f3ed4
henrylangman <henry.l.pta@gmail.com>
2020-07-01 00:49:41 +0200

    adding example of trickling dependencies

--- src/app/content/content.component.spec.ts
+++ src/app/content/content.component.spec.ts
@@ -1,6 +1,11 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContentComponent } from './content.component';
+import { ContentTopComponent } from './content-top/content-top.component';
+import { ContentLeftComponent } from './content-left/content-left.component';
+import { ContentRightComponent } from './content-right/content-right.component';
+import { ContentBottomComponent } from './content-bottom/content-bottom.component';
+import { FormsModule } from '@angular/forms';
 
 describe('ContentComponent', () => {
   let component: ContentComponent;
@@ -8,9 +13,16 @@ describe('ContentComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContentComponent ]
-    })
-    .compileComponents();
+      declarations: [
+        ContentComponent,
+
+        ContentTopComponent, // THESE ARE NOT MOCKED OUT!! Use MockComponent instead
+        ContentLeftComponent,
+        ContentRightComponent,
+        ContentBottomComponent,
+      ],
+      imports: [FormsModule], // THIS IS BAD! Mock out the dependencies
+    }).compileComponents();
   }));
 
   beforeEach(() => {

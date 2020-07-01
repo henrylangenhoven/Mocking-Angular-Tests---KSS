283d6d92767cd2be25c40bc45dc11902d600a63d
henrylangman <henry.l.pta@gmail.com>
2020-07-01 07:32:12 +0200

    adding example of proper use of HttpClientTestingModule

--- src/app/list.service.spec.ts
+++ src/app/list.service.spec.ts
@@ -1,12 +1,15 @@
 import { TestBed } from '@angular/core/testing';
 
 import { ListService } from './list.service';
+import { HttpClientModule } from '@angular/common/http';
 
 describe('ListService', () => {
   let service: ListService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      // imports: [HttpClientModule] // TODO Example 6: Proper use for HttpClientModule
+    });
     service = TestBed.inject(ListService);
   });
 

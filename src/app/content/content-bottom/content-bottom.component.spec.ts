e72b88423410fbd3889014f5b84e822dac5f35f8
henrylangman <henry.l.pta@gmail.com>
2020-07-01 07:31:16 +0200

    adding example mocking a service

--- src/app/content/content-bottom/content-bottom.component.spec.ts
+++ src/app/content/content-bottom/content-bottom.component.spec.ts
@@ -1,21 +1,34 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ContentBottomComponent } from './content-bottom.component';
+import { ListService } from '../../list.service';
+import { EMPTY, of } from 'rxjs';
 
 describe('ContentBottomComponent', () => {
   let component: ContentBottomComponent;
   let fixture: ComponentFixture<ContentBottomComponent>;
+  let listService: ListService; // TODO Example 1: Properly mocked service
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ ContentBottomComponent ]
-    })
-    .compileComponents();
+      declarations: [ContentBottomComponent],
+      providers: [
+        {
+          provide: ListService,
+          useValue: jasmine.createSpyObj('ListService', {
+            list: of(EMPTY),
+            updateListId: () => {},
+            deleteItem: () => {},
+          }),
+        },
+      ],
+    }).compileComponents();
   }));
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ContentBottomComponent);
     component = fixture.componentInstance;
+    listService = TestBed.inject(ListService as any);
     fixture.detectChanges();
   });
 
